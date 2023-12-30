a28244747027775a7fc1c9399f6c93e378c220ed
BoyfriendFNF <155038635+LakerWuzzy@users.noreply.github.com>
2023-12-30 18:38:28 -0300

    Tentando dnv

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -97,8 +97,7 @@ class MainMenuState extends MusicBeatState
 			menuItem.animation.addByPrefix('selected', optionShit[i] + " white", 24);
 			menuItem.animation.play('idle');
 			menuItem.ID = i;
-			//menuItem.screenCenter(X);
-			menuItem.x += 250;
+			menuItem.x = 250;
 			menuItems.add(menuItem);
 			menuItem.scrollFactor.set();
 			menuItem.antialiasing = true;
